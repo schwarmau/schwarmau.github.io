ec81554421a02d4c9f6cd7c9d8e616902499f886
Austin Schwarm <austin.schwarm@uconn.edu>
2020-03-28 14:41:22 -0400

    established a proper portfolio page with some placeholder projects

---
+++ assets/js/portfolio.js
@@ -0,0 +1,7 @@
+function setPortfolioProjectLabelAside(labelId) {
+    document.getElementById(labelId).classList.add("portfolio-project-label-aside");
+}
+
+function unsetPortfolioProjectLabelAside(labelId) {
+    document.getElementById(labelId).classList.remove("portfolio-project-label-aside");
+}
